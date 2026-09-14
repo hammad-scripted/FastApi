21ffbf1e6b4807da6ee126dbfb98b2386b967c78
Mohammad Hammad <hammad.scripted@gmail.com>
2026-09-14 15:41:29 +0530

    method added

--- 01_basics/basics.py
+++ 01_basics/basics.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
-app=FastAPI()
+
+app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/hammad")
+async def root():
+    return {
+        "message": "Hello Hammad",
+        "name": "Hammad",
+        "age": 25,
+        "gender": "male",
+        "country": "India",
+        "city": "New Delhi",
+    }
